81e0b3305666e06d93045429a1f1cc3803a33abc
coffeemug17 <mitulpandey2@gmail.com>
2023-04-09 09:06:16 +0100

    About Me complete

--- src/context/AboutMeContext.jsx
+++ src/context/AboutMeContext.jsx
@@ -1,8 +1,6 @@
 import { useState, createContext } from 'react';
 import { aboutMeData } from '../data/aboutMeData';
 import { clientsHeading as clientsPageHeading } from '../data/clientsData';
-import { clientsHeading2 as clientsSubHeading1 } from '../data/clientsData';
-import { clientsHeading3 as clientsSubHeading2 } from '../data/clientsData';
 import { clientsData as clientsDataJson } from '../data/clientsData';
 
 const AboutMeContext = createContext();
@@ -11,8 +9,6 @@ export const AboutMeProvider = ({ children }) => {
 	const [aboutMe, setAboutMe] = useState(aboutMeData);
 
 	const clientsHeading = clientsPageHeading;
-	const clientsHeading2 = clientsSubHeading1;
-	const clientsHeading3 = clientsSubHeading2;
 
 	const [clientsData, setClientsData] = useState(clientsDataJson);
 
@@ -22,8 +18,6 @@ export const AboutMeProvider = ({ children }) => {
 				aboutMe,
 				setAboutMe,
 				clientsHeading,
-				clientsHeading2,
-				clientsHeading3,
 				clientsData,
 				setClientsData,
 			}}
--- src/data/clientsData.js
+++ src/data/clientsData.js
@@ -1,55 +1,90 @@
-// Import images
-import SonyImage from '../images/brands/sony_gray.png';
-import AdidasImage from '../images/brands/adidas_gray.png';
-import FilaImage from '../images/brands/fila_gray.png';
-import NBImage from '../images/brands/nb_gray.png';
-import SamsungImage from '../images/brands/samsung_gray.png';
-import CanonImage from '../images/brands/canon_gray.png';
-import PumaImage from '../images/brands/puma_gray.png';
-
 export const clientsHeading = 'Technical Skills';
-export const clientsHeading2 = '';
-export const clientsHeading3 = 'My Skills';
+
 
 export const clientsData = [
 	{
 		id: 1,
-		title: 'Amazon',
+		title: 'HTML5',
 		img: "https://img.shields.io/badge/-HTML5-05122A?style=flat&logo=html5",
 	},
 	{
 		id: 2,
-		title: 'Sony',
-		img: SonyImage,
+		title: 'CSS3',
+		img: "https://img.shields.io/badge/-CSS-05122A?style=flat&logo=css3",
 	},
 	{
 		id: 3,
-		title: 'Adidas',
-		img: AdidasImage,
+		title: 'Python',
+		img: "https://img.shields.io/badge/-Python-05122A?style=flat&logo=python",
 	},
 	{
 		id: 4,
-		title: 'FILA',
-		img: FilaImage,
+		title: 'JavaScript',
+		img: "https://img.shields.io/badge/-JavaScript-05122A?style=flat&logo=javascript",
 	},
 	{
 		id: 5,
-		title: 'NB',
-		img: NBImage,
+		title: 'Django',
+		img: "https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=green",
 	},
 	{
 		id: 6,
-		title: 'SAMSUNG',
-		img: SamsungImage,
+		title: 'Express',
+		img: "https://img.shields.io/badge/-Express-05122A?style=flat&logo=express",
 	},
 	{
 		id: 7,
-		title: 'CANON',
-		img: CanonImage,
+		title: 'React',
+		img: "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
 	},
 	{
 		id: 8,
-		title: 'PUMA',
-		img: PumaImage,
+		title: 'Node',
+		img: "https://img.shields.io/badge/-Node.js-05122A?style=flat&logo=node.js",
+	},
+	{
+		id: 8,
+		title: 'MongoDB',
+		img: "https://img.shields.io/badge/-MongoDB-05122A?style=flat&logo=mongodb",
+	},
+	{
+		id: 8,
+		title: 'PostgreSQL',
+		img: "https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white",
+	},
+	{
+		id: 8,
+		title: 'Mongoose ODM',
+		img: "https://img.shields.io/badge/-Mongoose_ODM-05122A?style=flat&logo=mongodb",
+	},
+	{
+		id: 8,
+		title: 'TypeScript',
+		img: "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+	},
+	{
+		id: 8,
+		title: 'Git',
+		img: "https://img.shields.io/badge/-Git-05122A?style=flat&logo=git",
+	},
+	{
+		id: 8,
+		title: 'Bootstrap',
+		img: "https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white",
+	},
+	{
+		id: 8,
+		title: 'JWT',
+		img: "https://img.shields.io/badge/-JSON_Web_Tokens-05122A?style=flat&logo=jsonwebtokens",
+	},
+	{
+		id: 8,
+		title: 'Heroku',
+		img: "https://img.shields.io/badge/-Heroku-05122A?style=flat&logo=heroku",
+	},
+	{
+		id: 8,
+		title: 'AWS S3',
+		img: "https://img.shields.io/badge/-AWS_S3-05122A?style=flat&logo=amazons3",
 	},
 ];
